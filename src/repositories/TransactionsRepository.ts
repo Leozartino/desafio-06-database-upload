7a81b196e724229738e9a212710d13ea550e1183
Leozartino <leozartino@gmail.com>
2020-05-06 03:47:41 -0300

    feat(repositories): implementa TransactionRepo

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,6 +1,8 @@
-import { EntityRepository, Repository } from 'typeorm';
-
+/* eslint-disable no-param-reassign */
+import { EntityRepository, Repository, getCustomRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+import CategoryRepository from './CategoryRepository';
+import TransactionDTO from '../util/TransactionDTO';
 
 interface Balance {
   income: number;
@@ -8,10 +10,58 @@ interface Balance {
   total: number;
 }
 
+/**
+ * balance = numberIncome - numberOutcome;
+ */
+
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+
+    const balance: Balance = { income: 0, outcome: 0, total: 0 };
+
+    const result = transactions.reduce(
+      (previousBalance, { value, type }) => {
+        if (type === 'income') {
+          previousBalance.income += value;
+        } else {
+          previousBalance.outcome += value;
+        }
+        previousBalance.total =
+          previousBalance.income - previousBalance.outcome;
+        return previousBalance;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    Object.assign(balance, result);
+
+    return balance;
+  }
+
+  public async createTransaction({
+    title,
+    type,
+    value,
+    category,
+  }: TransactionDTO): Promise<Transaction> {
+    const categoriesRepository = getCustomRepository(CategoryRepository);
+
+    const getedCategory = await categoriesRepository.getCategory(category);
+
+    const transaction = this.create({
+      title,
+      type,
+      value,
+      category: getedCategory,
+    });
+
+    return transaction;
   }
 }
 
