1244e2c5b77c68ccff516013a8f64d0476f5eca2
Leozartino <leozartino@gmail.com>
2020-05-08 10:33:18 -0300

    feat: adiciona middleware de upload

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { getCustomRepository } from 'typeorm';
 import TransactionsRepository from '../repositories/TransactionsRepository';
-import CreateTransactionService from '../services/CreateTransactionService';
-import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import CreateTransactionService from '../services/CreateTransaction.service';
+import DeleteTransactionService from '../services/DeleteTransaction.service';
+import ImportTransactionsService from '../services/ImportTransactions.service';
+import uploadConfig from '../config/upload';
 
 const transactionsRouter = Router();
+const upload = multer(uploadConfig);
 
 transactionsRouter.get('/', async (request, response) => {
   const transactionsRepository = getCustomRepository(TransactionsRepository);
@@ -42,8 +45,16 @@ transactionsRouter.delete('/:id', async (request, response) => {
   return response.json({});
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const importTranasactions = new ImportTransactionsService();
+
+    const transactions = await importTranasactions.execute(request.file.path);
+
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
